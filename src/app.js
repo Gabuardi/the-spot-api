c3b5d07df059164a3e81ee9e3673372cae5d0850
Josué Gabuardi <jgabuardi@beenverified.com>
2020-07-18 00:57:41 -0600

    Implement MSSQL package

--- src/app.js
+++ src/app.js
@@ -1,57 +1,29 @@
 import express from 'express';
-import Connection from 'tedious/lib/connection.js';
-import Request from 'tedious/lib/request.js';
+import sql from 'mssql';
 
 const APP = express();
 const PORT = 3000;
 
-const DBCONFIG = {
-  authentication: {
-    options: {
-      userName: "server-boss",
-      password: "123Queso"
-    },
-    type: "default"
-  },
+const DB_CONFIG = {
+  user: "server-boss",
+  password: "123Queso",
   server: "ulacit-ws-the-spot.database.windows.net",
-  options: {
-    database: "TheSpotBusinessDB2",
-    encrypt: true
-  }
+  database: "TheSpotBusinessDB2",
 };
 
-const DBCONNECTION = new Connection(DBCONFIG);
-
-DBCONNECTION.on("connect", err => {
-  if (err) {
-    console.error(err.message);
+sql.connect(DB_CONFIG, error => {
+  if (error) {
+    console.error(error);
   } else {
-    queryDatabase();
+    console.log('✓ - CONNECTION POOL CREATED');
   }
 });
 
-function queryDatabase() {
-  console.log("Reading rows from the Table...");
-
-  // Read all rows from table
-  const request = new Request(
-    `SELECT TOP (1000) * FROM [dbo].[Management_User]`,
-    (err, rowCount) => {
-      if (err) {
-        console.error(err.message);
-      } else {
-        console.log(`${rowCount} row(s) returned`);
-      }
-    }
-  );
-
-  request.on("row", columns => {
-    columns.forEach(column => {
-      console.log("%s\t%s", column.metadata.colName, column.value);
-    });
-  });
-
-  DBCONNECTION.execSql(request);
-}
-
 APP.listen(PORT, () => console.log(`- || APP RUNNING ||--> http://localhost:${PORT}`));
+
+APP.get('/', (request, response) => {
+  let sqlRequest = new sql.Request();
+  sqlRequest.query('SELECT TOP (5) * FROM [SalesLT].[Product] FOR JSON PATH', (err, result) => {
+    response.send(result);
+  });
+});
