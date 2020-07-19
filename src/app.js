fa0031ad83e524dc080ce0bc40c1a8769d84a25d
Josué Gabuardi <jgabuardi@beenverified.com>
2020-07-18 21:56:09 -0600

    Add managementUser and role

--- src/app.js
+++ src/app.js
@@ -1,5 +1,6 @@
 import express from 'express';
 import sql from 'mssql';
+import bodyParser from 'body-parser';
 
 const APP = express();
 const PORT = 3000;
@@ -8,8 +9,11 @@ const DB_CONFIG = {
   user: "server-boss",
   password: "123Queso",
   server: "ulacit-ws-the-spot.database.windows.net",
-  database: "TheSpotBusinessDB2",
+  database: "TheSpotMVP",
+  parseJSON: true,
 };
+
+APP.use(bodyParser.json());
 
 sql.connect(DB_CONFIG, error => {
   if (error) {
@@ -21,9 +25,36 @@ sql.connect(DB_CONFIG, error => {
 
 APP.listen(PORT, () => console.log(`- || APP RUNNING ||--> http://localhost:${PORT}`));
 
-APP.get('/', (request, response) => {
+APP.get('/managementUser', (request, response) => {
   let sqlRequest = new sql.Request();
-  sqlRequest.query('SELECT TOP (5) * FROM [SalesLT].[Product] FOR JSON PATH', (err, result) => {
+  sqlRequest.query('SELECT * FROM Management_User FOR JSON PATH', (err, result) => {
     response.send(result);
   });
 });
+
+APP.post('/managementUser/new', (request, response) => {
+  let body = request.body;
+  let sqlRequest = new sql.Request();
+  sqlRequest.input('userName', body.userName);
+  sqlRequest.input('password', body.password);
+  sqlRequest.input('email', body.email);
+  sqlRequest.input('secQues', body.securityQuestion);
+  sqlRequest.input('secAns', body.securityAnswer);
+  sqlRequest.input('roleID', body.roleID);
+
+  sqlRequest.query(`INSERT INTO [Management_User] (userName, Password, Email, SecurityQuestion, SecurityAnswer, RoleID)` +
+    `VALUES (@userName, @password, @email, @secQues, @secAns, @roleID)`, (err, result) => {
+    response.send(result);
+  });
+});
+
+APP.post('/new/role', (request, response) => {
+  let body = request.body;
+  let sqlRequest = new sql.Request();
+  sqlRequest.input('roleName', body.roleName);
+  sqlRequest.query(`INSERT INTO [Roles] (Name)` +
+    `VALUES (@roleName)`, (err, result) => {
+    response.send(`Role ${body.roleName} created successfully`);
+  });
+});
+
