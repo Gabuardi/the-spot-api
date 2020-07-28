4158c67219114261d806932eceecf78ee33f6d58
Josué Gabuardi <jgabuardi@beenverified.com>
2020-07-27 22:00:54 -0600

    Add environment process port

--- src/app.js
+++ src/app.js
@@ -4,7 +4,7 @@ import base64 from 'base-64';
 import randomString from 'randomstring';
 
 const APP = express();
-const PORT = 3000;
+const PORT = process.env.PORT || 3000;
 
 const DB_CONFIG = {
   user: "server-boss",
