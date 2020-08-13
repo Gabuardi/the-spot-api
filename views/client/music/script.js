3c20b824673d220c0f0a2e8da5cf39468711d49d
Josué Gabuardi <jgabuardi@beenverified.com>
2020-08-13 02:20:08 -0600

    Payment (#20)

--- views/client/music/script.js
+++ views/client/music/script.js
@@ -1,45 +1,45 @@
-(function(){
-    $('.resource-card').click(function (){
-        let card = $(this);
-            $('#sidebar-artwork').css('background-image', `url("/music/artworks/${card.data('id')}.jpg")`);
-            $('#sidebar-title').text(card.data('title'));
-            $('#sidebar-artist').text(card.data('artist'));
-            $('#sidebar-genre').text(card.data('genres'));
-            $('#sidebar-album').text(card.data('album'));
-            $('#sidebar-year').text(card.data('year'));
-            $('#sidebar-record-label').text(card.data('recordlabel'));
-            $('.sidebar-dropdown').fadeIn(700);
-            $('.sidebar').addClass('active');
-            $('#buy-button').prop('href', `/payment?type=song&product=${card.data('id')}`);
-    });
-    
-    $('.sidebar-dropdown').click(() => {
-        $('.sidebar-dropdown').fadeOut(700);
-        $('.sidebar').removeClass('active');
-    });
+(function () {
+  $('.resource-card').click(function () {
+    let card = $(this);
+    $('#sidebar-artwork').css('background-image', `url("/music/artworks/${card.data('id')}.jpg")`);
+    $('#sidebar-title').text(card.data('title'));
+    $('#sidebar-artist').text(card.data('artist'));
+    $('#sidebar-genre').text(card.data('genres'));
+    $('#sidebar-album').text(card.data('album'));
+    $('#sidebar-year').text(card.data('year'));
+    $('#sidebar-record-label').text(card.data('recordlabel'));
+    $('.sidebar-dropdown').fadeIn(700);
+    $('.sidebar').addClass('active');
+    $('#buy-button').prop('href', `/payment?type=music&product=${card.data('id')}`);
+  });
 
-    $('#select-artist').change(function() {
-        let selected = $(this).val();
-        runNewUrl('artists', selected);
-    });
+  $('.sidebar-dropdown').click(() => {
+    $('.sidebar-dropdown').fadeOut(700);
+    $('.sidebar').removeClass('active');
+  });
 
-    $('#select-genre').change(function() {
-        let selected = $(this).val();
-        runNewUrl('genres', selected);
-    });
+  $('#select-artist').change(function () {
+    let selected = $(this).val();
+    runNewUrl('artists', selected);
+  });
 
-    $('#select-album').change(function() {
-        let selected = $(this).val();
-        runNewUrl('album', selected);
-    });
+  $('#select-genre').change(function () {
+    let selected = $(this).val();
+    runNewUrl('genres', selected);
+  });
 
-    $('#select-record-label').change(function() {
-        let selected = $(this).val();
-        runNewUrl('recordLabel', selected);
-    });
+  $('#select-album').change(function () {
+    let selected = $(this).val();
+    runNewUrl('album', selected);
+  });
 
-    $('#select-year').change(function() {
-        let selected = $(this).val();
-        runNewUrl('releaseYear', selected);
-    });
+  $('#select-record-label').change(function () {
+    let selected = $(this).val();
+    runNewUrl('recordLabel', selected);
+  });
+
+  $('#select-year').change(function () {
+    let selected = $(this).val();
+    runNewUrl('releaseYear', selected);
+  });
 })(jQuery)
