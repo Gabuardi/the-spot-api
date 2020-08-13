2f06fc0febafd4af057995b8a32cda321133be0f
Josué Gabuardi <jgabuardi@beenverified.com>
2020-08-13 01:54:28 -0600

    Implement payment

--- views/client/payment/script.js
+++ views/client/payment/script.js
@@ -5,3 +5,43 @@ $('.payment-method-card').click(function () {
   $('form').addClass('d-none');
   $(`#${dataForm}-form`).removeClass('d-none');
 });
+
+// ---------------------------------------------------------------------
+// FORM VALIDATION
+// ---------------------------------------------------------------------
+function errorParentPlacement(error, element) {
+  $(error).appendTo($(element).parent());
+}
+
+function donwloadFile(form, event) {
+  event.preventDefault();
+  window.location = $(form).data('download');
+}
+
+$('#credit-card-form').validate({
+  rules: {
+    cardNumber: 'required',
+    expMonth: 'required',
+    expYear: 'required',
+    cardProvider: 'required',
+    cvv: 'required'
+  },
+  messages: {
+    expMonth: 'This field is required',
+    expYear: 'This filed is required',
+    cardProvider: 'This field is required',
+  },
+  errorElement: 'p',
+  errorPlacement: errorParentPlacement,
+  submitHandler: donwloadFile
+});
+
+$('#easypay-form').validate({
+  rules: {
+    accountNumber: 'required',
+    securityCode: 'required',
+    password: 'required'
+  },
+  errorElement: 'p',
+  submitHandler: () => console.log('DOWNLOAD')
+});
